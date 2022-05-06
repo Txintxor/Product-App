dc48af3f28fb8c0196b5968479317edcfdf53f2f
AlexQ <78956966+Txintxor@users.noreply.github.com>
2022-05-06 14:08:23 +0200

    Add, delete & show methods ready. Delete modal ready. Add/Delete message setTimeout

--- app.js
+++ app.js
@@ -7,31 +7,74 @@ class Product {
 }
 
 class UI {
-  static counter = 1;
-
   addProduct(product) {
+    // let a = 2
+    // $('#addModal').show();
+    // document.getElementById('saveAdd').addEventListener('click', (e) =>{
+    //   let clck = e.target.id;
+    //   if (clck == 'saveAdd') {
+    //     a++ ;
+    //     clck = false;
+    //     const productList = document.getElementById("product-list");
+    //     const element = document.createElement("div");
+    //     element.innerHTML = `<ul class="list-unstyled d-flex flex-row justify-content-between py-2 px-4 align-items-center bg-info bg-opacity-25 fs-6 rounded-2 border-info">
+    //     <li class="">Item: ${product.item}</li>
+    //     <li class="">Price: ${product.price}</li>
+    //     <li class="">Year: ${product.year}</li>
+    //     <button class="btn btn-info btn-sm rounded-3 my-2 px-4 fs-6" onclick="deleteButton()">Delete</button>
+    //     </ul> `;
+    //     $('#addModal').hide();
+    //     return productList.appendChild(element);
+    //   }
+    //   else {
+    //     $('#addModal').hide();
+    //   }
+    // });
+
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
 
-    element.className = `div-${UI.counter}`;
-    UI.counter++;
-
     element.innerHTML = `<ul class="list-unstyled d-flex flex-row justify-content-between py-2 px-4 align-items-center bg-info bg-opacity-25 fs-6 rounded-2 border-info">
-    <li class="">Item: ${product.item}</li>
-    <li class="">Price: ${product.price}</li>
-    <li class="">Year: ${product.year}</li>
-    <button class="btn btn-info btn-sm rounded-3 my-2 px-4 fs-6">Save</button>
-    </ul> `;
+        <li class="">Item: ${product.item}</li>
+        <li class="">Price: ${product.price}</li>
+        <li class="">Year: ${product.year}</li>
+        <button class="btn btn-info btn-sm rounded-3 my-2 px-4 fs-6" onclick="deleteButton()">Delete</button>
+        </ul> `;
     productList.appendChild(element);
+
+    formId.reset();
+    this.showMessage("add");
+    // $('#addModal').show();
   }
 
-  deleteProduct() {
-
+  deleteProduct(product) {
+    $("#delModal").show();
+    document.getElementById("delModal").addEventListener("click", (e) => {
+      const clck = e.target.id;
+      if (clck == "saveDel") {
+        product.innerHTML = "";
+        $("#delModal").hide();
+        this.showMessage("del");
+      } else {
+        $("#delModal").hide();
+      }
+    });
   }
 
+  showMessage(info) {
+    const target = document.getElementById("message-bar");
+    if (info == "add") {
+      target.innerHTML =
+        '<p class="text-xl-start fs-4 p-2 bg-info bg-opacity-25">Item added</p>';
+    } else if (info == "del") {
+      target.innerHTML =
+        '<p class="text-xl-start fs-4 p-2 bg-warning bg-opacity-25">Item deleted</p>';
+    }
+    setTimeout(() => (target.innerHTML = ""), 3000);
+  }
 }
-
-document.getElementById("product-form").addEventListener("submit", (e) => {
+const formId = document.getElementById("product-form");
+formId.addEventListener("submit", (e) => {
   const item = document.getElementById("item").value;
   const price = document.getElementById("price").value;
   const year = document.getElementById("year").value;
@@ -43,6 +86,10 @@ document.getElementById("product-form").addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-document.getElementById("product-list").addEventListener("click", (e) => {
-  
-});
+function deleteButton() {
+  document.getElementById("product-list").addEventListener("click", (e) => {
+    const ui = new UI();
+    const product = e.target.parentElement.parentElement;
+    ui.deleteProduct(product);
+  });
+}
